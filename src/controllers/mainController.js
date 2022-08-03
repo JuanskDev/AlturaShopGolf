0992f9db52877ec56bd92e838ea2564d4cbb4f11
JuanskDev <juanscarafia7@gmail.com>
2022-08-03 12:20:31 +0800

    Prueba sprint 4

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -4,15 +4,15 @@ const mainController = {
     index: (req,res) => {
         res.render(path.join(__dirname, '../views/home.ejs'))
     },
-    head: (req,res) => {
-        res.render(path.join(__dirname, '../views/partials/head.ejs'))
-    },
-    header: (req,res) => {
-        res.render(path.join(__dirname, '../views/partials/header.ejs'))
-    },
-    footer: (req,res) => {
-        res.render(path.join(__dirname, '../views/partials/footer.ejs'))
-    }
+    // head: (req,res) => {
+    //     res.render(path.join(__dirname, '../views/partials/head.ejs'))
+    // },
+    // header: (req,res) => {
+    //     res.render(path.join(__dirname, '../views/partials/header.ejs'))
+    // },
+    // footer: (req,res) => {
+    //     res.render(path.join(__dirname, '../views/partials/footer.ejs'))
+    // }
 };
 
 module.exports = mainController;
--- src/routes/main.js
+++ src/routes/main.js
@@ -6,8 +6,8 @@ const mainController = require('../controllers/mainController');
 var router = express.Router();
 
 router.get('/' , mainController.index);
-router.get('/head' , mainController.head);
-router.get('/header' , mainController.header);
-router.get('/footer' , mainController.footer);
+// router.get('/head' , mainController.head);
+// router.get('/header' , mainController.header);
+// router.get('/footer' , mainController.footer);
 
 module.exports = router;
