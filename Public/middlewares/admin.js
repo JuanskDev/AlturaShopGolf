e0a91641199d0472f07754b0b20bc11d4ca70635
acontrer31 <agustincontrerasvidal@gmail.com>
2022-08-05 00:15:41 -0300

    cambios generales

---
+++ Public/middlewares/admin.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const admins = ['Ada' , 'Greta' , 'Vim' , 'Tim'];
+
+
+function admin(req, res, next){
+    let user = req.query.user;
+    if(user){
+        admins.forEach(function(admin){ // Probar con filter o indexOf
+            if(user == admin){
+                next();
+            }
+            else {
+                res.send('No tienes los privilegios para ingresar');
+            };
+        });
+    }
+    };
+
+
+module.exports = admin;
