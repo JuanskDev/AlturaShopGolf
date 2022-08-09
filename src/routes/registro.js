6292fabe71df21b29271e5cb607543e0b9ab7f97
acontrer31 <agustincontrerasvidal@gmail.com>
2022-08-09 12:37:44 -0300

    changes registro.js

--- src/routes/registro.js
+++ src/routes/registro.js
@@ -9,16 +9,17 @@ router.get('/registro', controller.index);
 router.post('/registro',[
     check('nombre').isLength({min:5}).withMessage('Debe ingresar un nombre'),
     check('apellido').isLength({min:1}).withMessage('Debe ingresar un apellido'),
-    
-    // check('email').isEmail().withMessage('Debe ingresar un email valido'),
-    // check('color').isLength({min:1}).withMessage('Debe seleccionar un color'),
-    // body('age').custom(value => {
-    //     if(isNaN(value)){
-    //         throw new Error('El valor ingresado debe ser un numero');
-    //     }else {
-    //         return true;
-    //     }
-    // })
+    check('email').isEmail().withMessage('Debe ingresar un email valido'),
+    check('confirm-email').isEmail().withMessage('Debe confirmar su direccion de email'),
+    check('password').isLength({min:6}).withMessage('Debe ingresar una contraseña con un minimo de 6 caracteres'),
+    check('confirmar-password').isLength({min:6}).withMessage('Debe ingresar nuevamente su contraseña'),
+    body('telefono').custom(value => {
+        if(isNaN(value)){
+            throw new Error('El valor ingresado deben ser numeros');
+        }else {
+            return true;
+        }
+    })
 
 
 ], controller.store);
